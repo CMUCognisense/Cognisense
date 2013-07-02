ba3725cde99f752195d4ac939a4d1150bd89155a
Parth Mehta <parthmehta@cmu.edu>
2013-07-02 15:27:25 -0400

    added build and ru script

--- RaspberryPI/TestScheme.java
+++ RaspberryPI/TestScheme.java
@@ -17,21 +17,23 @@ public class TestScheme {
 		sdl.addLocation(serviceId);
 		sdl.registerActions(serviceId, "giveInfo", TestScheme.class);
 		sdl.registerActions(serviceId, "getInfo", TestScheme.class);
-		sdl.registerActions(serviceId2, "giveInfo", TestScheme.class);
-		sdl.registerActions(serviceId2, "getInfo", TestScheme.class);
-		sdl.registerActions(serviceId3, "giveInfo", TestScheme.class);
-		sdl.registerActions(serviceId3, "getInfo", TestScheme.class);
+		sdl.registerActions(serviceId2, "giveInfo2", TestScheme.class);
+		sdl.registerActions(serviceId2, "getInfo2", TestScheme.class);
+		sdl.registerActions(serviceId3, "giveInfo3", TestScheme.class);
+		sdl.registerActions(serviceId3, "getInfo3", TestScheme.class);
 		
 		
         Message message = new Message(serviceId);
         message.addAction("giveInfo","input");
         message.addServiceType("Test");
+        message.addServiceType("Test1");
 		
 		sdl.testOnReceiveMessage(message);
 
 		message = new Message(serviceId);
-        message.addAction("getInfo","input");
+        message.addAction("getInfo","abc ");
         message.addServiceType("Test");
+        message.addServiceId(serviceId3);
 		
 		sdl.testOnReceiveMessage(message);
 
@@ -40,10 +42,29 @@ public class TestScheme {
 
 	public void giveInfo(Object actionInput, Object srcServiceId) {
 		System.out.println("GiveInfo is called " + (String)actionInput + (String)srcServiceId );
+		
 	}
 	
 	public void getInfo(Object actionInput, Object srcServiceId) {
 		System.out.println("GetInfo is called " + (String)actionInput + (String)srcServiceId);
+	}
+	
+	public void giveInfo2(Object actionInput, Object srcServiceId) {
+		System.out.println("GiveInfo2 is called " + (String)actionInput + (String)srcServiceId );
+		
+	}
+	
+	public void getInfo2(Object actionInput, Object srcServiceId) {
+		System.out.println("GetInfo2 is called " + (String)actionInput + (String)srcServiceId);
+	}
+	
+	public void giveInfo3(Object actionInput, Object srcServiceId) {
+		System.out.println("GiveInfo3 is called " + (String)actionInput + (String)srcServiceId );
+		
+	}
+	
+	public void getInfo3(Object actionInput, Object srcServiceId) {
+		System.out.println("GetInfo3 is called " + (String)actionInput + (String)srcServiceId);
 	}
 	
 }
