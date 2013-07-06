3bfa632a7ddf8867e83e7356b583a082735d8b08
Jiahan Wang <609107922@qq.com>
2013-07-05 22:07:37 -0400

    Add LEDConfigurationService & LEDNotificationProxy & LEDStatus Property

--- RaspberryPI/Doorbell.java
+++ RaspberryPI/Doorbell.java
@@ -1,35 +1,36 @@
 import servicediscovery.Message;
 import servicediscovery.ServiceDiscoveryLayer;
 
-
 public class Doorbell {
 	int countNumOfReplies;
 	static String serviceId;
 	static ServiceDiscoveryLayer sdl;
-	 public static void main(String[] args) throws Exception{
-	        sdl = new ServiceDiscoveryLayer(true,false);
-	        sdl.registerApp(new Doorbell());
-	        serviceId = sdl.registerNewService("Doorbell");
-	        sdl.addLocationProperty(serviceId);
-	        sdl.registerActions(serviceId, "infoMethod","giveInfo", Doorbell.class);
-	        
-	    }
 
-	 public void infoMethod(Object actionInput, Object srcServiceId) {
-	        // this will print out the message sent by the doorbell.
-	        String location = sdl.getProperties(serviceId).get("Location").toString();
-	        String trigger = sdl.getActions(serviceId).toString();
-	        
-	        Message message = new Message(serviceId);
-	        message.addTrigger("onDoorbell", location+":"+trigger);
-	        message.addServiceId((String)srcServiceId);
-	    	
-	        sdl.sendMessage(message);
-	        
-	    }
-	 
-	 public void onLocationFound(Object parameters) {
-		 countNumOfReplies++;
-	 }
-	 
+	public static void main(String[] args) throws Exception {
+		sdl = new ServiceDiscoveryLayer(true, false);
+		sdl.registerApp(new Doorbell());
+		serviceId = sdl.registerNewService("Doorbell");
+		sdl.addLocationProperty(serviceId);
+		sdl.registerActions(serviceId, "infoMethod", "giveInfo", Doorbell.class);
+
+	}
+
+	public void infoMethod(Object actionInput, Object srcServiceId) {
+		// this will print out the message sent by the doorbell.
+		String location = sdl.getProperties(serviceId).get("Location")
+				.toString();
+		String trigger = sdl.getActions(serviceId).toString();
+
+		Message message = new Message(serviceId);
+		message.addTrigger("onDoorbell", location + ":" + trigger);
+		message.addServiceId((String) srcServiceId);
+
+		sdl.sendMessage(message);
+
+	}
+
+	public void onLocationFound(Object parameters) {
+		countNumOfReplies++;
+	}
+
 }
